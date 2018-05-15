7adcf3373fb78fef9d6fda7abb2a0feb75b6ccf2
Peter Stenger <peter.a.stenger@gmail.com>
2018-05-15 08:05:54 -0400

    Update gopowerschool.go

--- gopowerschool.go
+++ gopowerschool.go
@@ -12,14 +12,15 @@ func Client(url string) *PublicPortalServiceJSONPortType {
 	wsdl_url := fmt.Sprintf("%s/pearson-rest/services/PublicPortalServiceJSON?wsdl", url)
 	return NewPublicPortalServiceJSONPortType(wsdl_url, true, &auth)
 }
-func (client *PublicPortalServiceJSONPortType) GetStudent(username, password string) (*StudentDataVO, error) {
+func (client *PublicPortalServiceJSONPortType) CreateUserSessionAndStudent(username, password string) (*UserSessionVO, int64, error) {
+	
 	PublicPortalLogin := LoginToPublicPortal{Username: username, Password: password}
 	response, err := client.LoginToPublicPortal(&PublicPortalLogin)
 	if err != nil {
-		return nil, err
+		return nil, 0, err
 	}
 	if response.Return_.MessageVOs != nil {
-		return nil, fmt.Errorf("error: %s - %s", response.Return_.MessageVOs[0].Title, response.Return_.MessageVOs[0].Description)
+		return nil, 0, fmt.Errorf("error: %s - %s", response.Return_.MessageVOs[0].Title, response.Return_.MessageVOs[0].Description)
 	}
 	newSession := UserSessionVO{
 		UserId:            response.Return_.UserSessionVO.UserId,
@@ -27,8 +28,14 @@ func (client *PublicPortalServiceJSONPortType) GetStudent(username, password str
 		ServerInfo:        &ServerInfo{ApiVersion: response.Return_.UserSessionVO.ServerInfo.ApiVersion},
 		ServerCurrentTime: response.Return_.UserSessionVO.ServerCurrentTime,
 		UserType:          response.Return_.UserSessionVO.UserType}
-
-	studentDataArguments := GetStudentData{UserSessionVO: &newSession, StudentIDs: []int64{int64(response.Return_.UserSessionVO.StudentIDs[0])}, Qil: &QueryIncludeListVO{Includes: []int32{1}}}
+	return newSession, int64(response.Return_.UserSessionVO.StudentIDs[0]), nil
+}
+func (client *PublicPortalServiceJSONPortType) GetStudent(username, password string) (*StudentDataVO, error) {
+	session, userID, err := client.CreateUserSessionAndStudent(username, password)
+	if err != nil {
+		return nil, err
+	}
+	studentDataArguments := GetStudentData{UserSessionVO: &session, StudentIDs: []int64{studentID}, Qil: &QueryIncludeListVO{Includes: []int32{1}}}
 	student, err := client.GetStudentData(&studentDataArguments)
 	if err != nil {
 		return nil, err
